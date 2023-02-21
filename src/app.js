0505f697769b45bbed25f69a47cb4836cc699894
catalinasehrt <catalina.dali94@gmail.com>
2023-02-21 17:02:24 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function onLoad(who, what, when) {
+    let generateLetterAleatory = GenerateRandom(who);
+    let generateLetterAleatory2 = GenerateRandom(what);
+    let generateLetterAleatory3 = GenerateRandom(when);
+
+    return (
+      generateLetterAleatory +
+      " " +
+      generateLetterAleatory2 +
+      " " +
+      generateLetterAleatory3
+    );
+  }
+
+  function GenerateRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  document.getElementById("excuse").innerHTML = onLoad(who, what, when);
 };
